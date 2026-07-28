f985f29724d7712f8d5f589e9eb800c30de9a7c2
Manoj Kumar Myaka <manojkumar@gmail.com>
2026-07-28 13:02:40 +0530

    feat: add React counter application

---
+++ exercises/day-7-react/src/App.jsx
@@ -0,0 +1,12 @@
+import Counter from "./components/Counter";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Counter title="React Counter App" />
+    </>
+  );
+}
+
+export default App;
